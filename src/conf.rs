78c62b82580a4b2ef29048f687a06dfa6cd01c2b
Chris J Arges <christopherarges@gmail.com>
2015-06-10 15:50:17 -0500

    Conf: Add functional configuration loader
    Now we can load a configuration file from '~/.ruffrc'.
    
    Signed-off-by: Chris J Arges <christopherarges@gmail.com>

--- src/conf.rs
+++ src/conf.rs
@@ -1,30 +1,78 @@
-/* configuration code
-
-use toml format since it is pretty standard in rust
-
-[colors]
-  theme = dark
-
-[keys]
-  style = vim
-
-[account]
-  [account.name]
-    imap_user = "user@host.com"
-    smtp_url = ""
-*/
+#![allow(dead_code)]
+//! Conf - configuration file loading and parsing
+//! Author: Chris J Arges <christopherarges@gmail.com>
+//!
+//! # Examples
+//! ```toml
+//! [theme]
+//!  colors = dark
+//!
+//! [keys]
+//!  style = vim
+//!
+//! [accounts]
+//!   [accounts.default]
+//!     imap_user = "user@host.com"
+//!     smtp_url = ""
+//! ```
 
 extern crate toml;
-#[allow(unused_imports)]
 use self::toml::*;
+use std::fs::File;
+use std::io::prelude::*;
+use std::env;
 
-#[allow(dead_code)]
-const CONF_PATH:&'static str= "~/.ruffrc";
+const CONF_FILE:&'static str= ".ruffrc";
 
-pub struct Conf;
+pub struct Conf {
+    conf: toml::Value,
+}
 
 impl Conf {
+    /// Loads a new configuration file
+    fn load() -> toml::Value {
+        /* load the configuration file */
+        let path = env::home_dir().unwrap().join(CONF_FILE);
+        let mut file = match File::open(&path) {
+            Ok(file) => file,
+            Err(..) => { panic!("Configuration file: {:?} not found.", path); }
+        };
+        let mut s = String::new();
+        match file.read_to_string(&mut s) {
+            Ok(..) => {}
+            Err(..) => { panic!("Unable to read configuration file: {:?}.", path); }
+        }
+
+        /* parse the string and nicely print any errors */
+        let mut parser = toml::Parser::new(&s[..]);
+        let toml = match parser.parse() {
+            Some(value) => value,
+            None => {
+                for err in &parser.errors {
+                    let (loline, locol) = parser.to_linecol(err.lo);
+                    let (hiline, hicol) = parser.to_linecol(err.hi);
+                    println!("{:?}:{}:{}-{}:{} error: {:?}",
+                             path, loline, locol, hiline, hicol, err.desc);
+               }
+               panic!("Malformed Configuration File {:?}", path); }
+        };
+
+        /* return parsed configuration */
+        Value::Table(toml)
+    }
+
+    /// Loads a configuration and constructions a new Conf object
     pub fn new() -> Conf {
-        Conf
+        Conf { conf: Conf::load() }
+    }
+
+    /// Reloads a new configuration into Conf object
+    pub fn reload(&mut self) {
+        self.conf = Conf::load();
+    }
+
+    /// Lookup key from Conf object
+    pub fn lookup(&self, key: &'static str) -> &toml::Value {
+        self.conf.lookup(key).unwrap()
     }
 }
